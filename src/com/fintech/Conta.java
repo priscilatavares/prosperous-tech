ffdd18a872afd70b876ee95a2281f6269d214665
Priscila Tavares <rm550557@fiap.com.br>
2023-08-27 13:15:42 -0300

    AlteraConta

--- src/com/fintech/Conta.java
+++ src/com/fintech/Conta.java
@@ -1,5 +1,17 @@
 package com.fintech;
 
 public class Conta {
+	int idConta;
+	int numeroConta;
+	double saldoConta;
+	int tipoConta;
+	
+	
+	public void depositar(double valor) {
+		saldoConta = saldoConta + valor;
+	}
+	public void sacar(double valor) {
+		saldoConta = saldoConta - valor;
+	}
 
 }
