39c62de1a8007e0ef6561a9fa85a87cd60503945
Priscila Tavares <rm550557@fiap.com.br>
2023-08-27 13:59:24 -0300

    Alterando conta e adc teste

--- src/com/fintech/Conta.java
+++ src/com/fintech/Conta.java
@@ -6,12 +6,44 @@ public class Conta {
 	double saldoConta;
 	int tipoConta;
 	
+	public Conta() {
+		
+	}
+	
+	public int getIdConta() {
+		return idConta;
+	}
+	public void setIdConta(int idConta) {
+		this.idConta = idConta;
+	}
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	public double getSaldoConta() {
+		return saldoConta;
+	}
+	public void setSaldoConta(double saldoConta) {
+		this.saldoConta = saldoConta;
+	}
+	public int getTipoConta() {
+		return tipoConta;
+	}
+	public void setTipoConta(int tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+	
 	
 	public void depositar(double valor) {
 		saldoConta = saldoConta + valor;
 	}
 	public void sacar(double valor) {
 		saldoConta = saldoConta - valor;
+	}
+	public double recuperarSaldo() {
+		return saldoConta;
 	}
 
 }
--- src/com/fintech/Teste.java
+++ src/com/fintech/Teste.java
@@ -1,5 +1,21 @@
 package com.fintech;
 
 public class Teste {
+	public static void main(String[] args) {
+		
+		Conta conta = new Conta();
+		
+		conta.idConta = 1;
+		conta.numeroConta = 2 ;
+		conta.saldoConta = 100;
+		conta.tipoConta = 3;
+		
+		conta.depositar(500);
+		
+		System.out.println(conta.saldoConta);
+		
+		
+		
+	}
 
 }
